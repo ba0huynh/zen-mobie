3dd636b90f291d2be8eca3a19c989c89686cc24a
Bao Huynh <124868331+ba0huynh@users.noreply.github.com>
2026-07-31 16:44:03 +0700

    a

--- env.ts
+++ env.ts
@@ -1,7 +1,7 @@
 const env = {
     API_URL: process.env.EXPO_PUBLIC_API_URL,
-    CDN_URL: process.env.EXPO_PUBLIC_CDN_URL,
-    WEBSITE_URL: process.env.EXPO_PUBLIC_WEBSITE_URL,
+    // CDN_URL: process.env.EXPO_PUBLIC_CDN_URL,
+    // WEBSITE_URL: process.env.EXPO_PUBLIC_WEBSITE_URL,
     NODE_ENV: process.env.EXPO_PUBLIC_NODE_ENV,
 };
 
